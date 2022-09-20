9e688b9ab3073d0e06746e72002d2daa33fe7e5d
Andrew Tran <andrew_0414@hotmail.ca>
2022-09-20 18:55:12 -0400

    completed task 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let temp_string = string;
+    for (let i =1; i<num; i++){ 
+        string = string.concat(temp_string);
 
+    }   
+    if (num ==0){ 
+        string ='';
+    } else if (num <0){ 
+        string ='ERROR';
+    }
+    return string;
 };
 
 // Do not edit below this line
