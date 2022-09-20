32cadbbd21e9c9a92d5a38b73dd451de3cab93c3
Andrew Tran <andrew_0414@hotmail.ca>
2022-09-20 19:22:10 -0400

    Completed task 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let temp_array = new Array(string.length); 
+let temp_string= '';
+let counter =0;
+for (let i = string.length; i>0; i--){ 
+    temp_array[counter] = string.slice(i-1,i);
+    counter++;
+}
+temp_string=temp_array.join('');
+return temp_string;
 };
 
 // Do not edit below this line
