532a62f15c49589098acf4c7bc53c1bb6ff32d37
Andrew Tran <andrew_0414@hotmail.ca>
2022-09-20 21:14:39 -0400

    completed task -04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr1, ...rand) {
+    rand.sort();
+    for (let i=0; i <arr1.length; i++){ 
+        for (let j=0; j<rand.length; j++){ 
+            if (rand[j]==arr1[i] && typeof(rand[j])== typeof(arr1[i])){ 
+               arr1.splice(i,1);
+            }
+        }
+    }
+    return arr1;
 };
 
 // Do not edit below this line
