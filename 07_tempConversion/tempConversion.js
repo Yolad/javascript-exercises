c31148214572e8941e7dd5e85cbd54d945bbc9bf
Andrew Tran <andrew_0414@hotmail.ca>
2022-09-20 22:34:08 -0400

    completed task 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(faren) {
+let cel = (faren-32) * (5/9);
+return Math.round(cel * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+let faren = (cel *(9/5)) +32;
+return Math.round(faren * 10) / 10;;
 };
 
 // Do not edit below this line
